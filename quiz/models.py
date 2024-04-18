5dad4b812afec0cc01c8fb089c2d77c4990a3679
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2024-04-19 01:36:11 +0300

    topic model added

--- quiz/models.py
+++ quiz/models.py
@@ -11,6 +11,13 @@ class Category(models.Model):
         return self.name
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Question(models.Model):
     question = models.TextField(blank=False, unique=True)
